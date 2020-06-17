44fa46bbc2d6c55e43b260ba8f89d339c9e156e1
Ramon Sena <ramonsenadev@gmail.com>
2020-06-17 09:29:18 -0300

    Gerencia repositorios  cadastrados no  back-end com node

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(({ data }) => {
+        setRepositories(data)
+      })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post('/repositories', {
+      title: `Project with ReactJS ${Date.now()}`,
+      owner: 'Ramon Sena'
+    })
+    setRepositories([...repositories, data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+    setRepositories(repositories.filter((repository) => repository.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
